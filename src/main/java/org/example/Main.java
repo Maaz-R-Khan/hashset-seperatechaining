7e4179f980abb4fd53f7ba9b04b20b79bcb38335
Maaz Khan <63423339+Maaz-R-Khan@users.noreply.github.com>
2025-03-04 10:36:37 -0500

    updated add method and added hash set implementation in main method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,14 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
+        MyHashSet hashSet = new MyHashSet();
+        hashSet.add(1);
+        hashSet.add(20);
+        hashSet.add(30);
+        hashSet.add(40);
+        hashSet.add(50);
+
+        System.out.println(hashSet);
 
 
     }
--- src/main/java/org/example/MyHashSet.java
+++ src/main/java/org/example/MyHashSet.java
@@ -4,26 +4,55 @@ import java.util.ArrayList;
 
 public class MyHashSet {
 
-    int arr[];
-    ArrayList<Integer> bucket;
-    int numElements;
+   private ArrayList<Integer>[] buckets;
+   private int numElements;
 
+    // Default constructor
     MyHashSet() {
-        arr = arr[numElements];
-        bucket = new ArrayList<>(4); //
+        numElements = 0; // we set the numElements to zero, because there is no data yet.
+        buckets = new ArrayList[4]; //declaration of an array list.
+
+        //Initializes each bucket
+        for (int i = 0; i < buckets.length; i++) {
+            buckets[i] = new ArrayList<>();
+        }
     }
 
     // hasItem method
     boolean hasItem(int targetItem) {
-        return true;
+        for (int i = 0; i < buckets.length; i++) {
+            if (buckets[i].contains(targetItem)) {
+                return true;
+            }
+        }
+    return false;
     }
 
     // add method
-    void add(int item) {}
+   public void add(int data) {
+
+        /* Pseudocode for adding a key/value pair to the hash table.
+        * index = hash(key)
+        * list = hashTable[index]
+        * Add the key/value pair to list
+        * */
+
+       //Get the bucket index(hashing)
+
+       int index  = data % buckets.length;
+
+       // Get the bucket using the bucket index
+       ArrayList<Integer> currentBucket;
+       currentBucket = buckets[index];
+       currentBucket.add(data);
+    }
 
     double loadFactor() {
+    return 0;
     }
 
     void resize(int newSize){}
+
+
 
 }
