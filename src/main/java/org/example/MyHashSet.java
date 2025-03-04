2575c04af2dfddeeff24b03126a30093a63e6055
Maaz Khan <63423339+Maaz-R-Khan@users.noreply.github.com>
2025-03-04 09:54:05 -0500

    created MyHashSet starter code

---
+++ src/main/java/org/example/MyHashSet.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class MyHashSet {
+
+    int arr[];
+    ArrayList<Integer> bucket;
+    int numElements;
+
+    MyHashSet() {
+        arr = arr[numElements];
+        bucket = new ArrayList<>(4); //
+    }
+
+    // hasItem method
+    boolean hasItem(int targetItem) {
+        return true;
+    }
+
+    // add method
+    void add(int item) {}
+
+    double loadFactor() {
+    }
+
+    void resize(int newSize){}
+
+}
